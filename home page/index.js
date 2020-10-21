0be32a4ea082e64b5d69f2290d30a0ad604fbd97
Hallail <63903484+Hallail@users.noreply.github.com>
2020-10-21 17:49:43 +0300

    Fixed error and changed to scrolling

--- home page/index.js
+++ home page/index.js
@@ -3,24 +3,26 @@ let y = 0;
 let selectedText = document.querySelector("#HomeThing");
 
 
-//window.addEventListener('scroll', scrolling);
+window.addEventListener('scroll', scrolling);
 
-document.addEventListener("wheel", mouseScroll);
+//document.addEventListener("wheel", mouseScroll);
 
-function scrolling(event){
+function scrolling(){
     let delta = window.scrollY;
-    let maxDelta = 300;
+    let maxDelta = 200;
     if (delta < maxDelta) {selectedText.style.opacity = (maxDelta - delta)/maxDelta;}
     else {selectedText.style.opacity = 0;}
-    console.log(selectedText.style.opacity); 
+    //console.log(selectedText.style.opacity); 
 }
 
 let delta = 0;
 
-function mouseScroll(event){
+function mouseScroll(event){ //For Testing Purposes
     if (!(delta <= 0 & event.deltaY < 0)) delta += event.deltaY;
     let maxDelta = 1000;
-    if (delta < maxDelta) {selectedText.style.opacity = (maxDelta - delta)/maxDelta;}
+    if (delta < maxDelta) {
+        selectedText.style.opacity = (maxDelta - delta)/maxDelta;
+    }
     else {selectedText.style.opacity = 0;}
     console.log(event);
 }
