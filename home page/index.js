02f085609c63d61531033149607c39d1fe4d5e4c
Hallail <63903484+Hallail@users.noreply.github.com>
2020-10-21 17:39:28 +0300

    Fading away

--- home page/index.js
+++ home page/index.js
@@ -1,12 +1,26 @@
 let y = 0;
 
-let selectedText = document.getElementsByTagName('h2');
+let selectedText = document.querySelector("#HomeThing");
 
 
-window.addEventListener('scroll', scrolling);
+//window.addEventListener('scroll', scrolling);
+
+document.addEventListener("wheel", mouseScroll);
 
 function scrolling(event){
     let delta = window.scrollY;
     let maxDelta = 300;
-    selectedText.opacity = () 
+    if (delta < maxDelta) {selectedText.style.opacity = (maxDelta - delta)/maxDelta;}
+    else {selectedText.style.opacity = 0;}
+    console.log(selectedText.style.opacity); 
+}
+
+let delta = 0;
+
+function mouseScroll(event){
+    if (!(delta <= 0 & event.deltaY < 0)) delta += event.deltaY;
+    let maxDelta = 1000;
+    if (delta < maxDelta) {selectedText.style.opacity = (maxDelta - delta)/maxDelta;}
+    else {selectedText.style.opacity = 0;}
+    console.log(event);
 }
